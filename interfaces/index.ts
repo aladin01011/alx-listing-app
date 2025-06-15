2ae8004c7b9abfd6e4974ad7a088fe75278c664e
aladin <143756798+aladin01011@users.noreply.github.com>
2025-06-15 01:04:48 +0100

    Update index.ts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -9,3 +9,24 @@ export interface ButtonProps {
   onClick: () => void;
   disabled?: boolean;
 }
+export interface PropertyProps {
+  name: string;
+  rating: number;
+  address: {
+    city: string;
+    country: string;
+  };
+  image: string;
+  images?: string[];
+  description: string;
+  category: string[];
+  price: number;
+  reviews: ReviewProps[];
+}
+
+export interface ReviewProps {
+  name: string;
+  avatar: string;
+  rating: number;
+  comment: string;
+}
